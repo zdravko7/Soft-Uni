21e08c42e243ad3233e7a9e8c131c4a8dc009a3b
Zdravko Botushanov <zdravko@zdravko.be>
2015-01-18 19:05:34 +0200

    C# Math

--- C#/C# Math/SomeFactorial/SomeFactorial.cs
+++ C#/C# Math/SomeFactorial/SomeFactorial.cs
@@ -1,9 +1,6 @@
 ﻿// This program finds the factorial of a given number
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text.RegularExpressions;
 using System.Numerics;
 
 public class SomeFactorials
@@ -11,13 +8,12 @@ public class SomeFactorials
     public static void Main(string[] args)
     {
         Console.Write("Enter your number: ");
-        double number = Convert.ToDouble(Console.ReadLine());
-        BigInteger result = new BigInteger();
-        result = 1;
+        int number = int.Parse(Console.ReadLine());
+        BigInteger result = 1;
 
-        while (number != 0)
+        while (number > 1)
         {
-            result = result * (BigInteger) number;
+            result = result * number;
             number--;
         }
 
